c63099749b0fe49996300c07c0117ed6bebc61fa
ikamean <ika.bidzishvili@gmail.com>
2021-11-15 00:06:33 +0400

    no proxy for heroku :D

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const call = async ({ url, id = '' }) => {
-  const res = await axios.get(`${url}/${id}`);
+  const baseUrl = process.env.REACT_APP_STRAPI_BASE_URL;
+  const res = await axios.get(`${baseUrl}/${url}/${id}`);
 
   return res.data;
 };
